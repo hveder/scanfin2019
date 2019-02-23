bf2c08d3cbaecd45684c205ab0e83872fc24c0ac
Hendrik Veder <h.veder@webmobil24.com>
2019-02-23 15:36:04 +0100

    Packiging

--- src/app/takevinphotos/takevinphotos.module.ts
+++ src/app/takevinphotos/takevinphotos.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-
 import { TakevinphotosPage } from './takevinphotos.page';
 import {MatButtonModule, MatCheckboxModule, MatCardModule, MatExpansionModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
